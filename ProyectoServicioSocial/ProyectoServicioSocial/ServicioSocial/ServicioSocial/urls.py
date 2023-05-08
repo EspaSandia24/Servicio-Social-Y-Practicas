cecbc9766d3765fd23d981ac7ae6f5888879525c
Andrea Castro <35165623@uaz.edu.mx>
2023-05-08 13:10:22 -0600

    Atualizacion de Cruds Alumnos, se acomodo el editar

--- ProyectoServicioSocial/ProyectoServicioSocial/ServicioSocial/ServicioSocial/urls.py
+++ ProyectoServicioSocial/ProyectoServicioSocial/ServicioSocial/ServicioSocial/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('ususarios/', include('ususarios.urls')),
     path('programas/', include('instituciones.urls_programas')),
     path('unidades/', include('instituciones.urls_unidades')),
-    path('reportes_iniciales/', include('reportes.urls_reporteI')),
+   path('reportes_iniciales/', include('reportes.urls_reporteI')),
     path('instituciones/', include('instituciones.urls_instituciones')),
     path('alumnos/', include('alumnos.urls')),
     
--- ProyectoServicioSocial/ProyectoServicioSocial/ServicioSocial/alumnos/urls.py
+++ ProyectoServicioSocial/ProyectoServicioSocial/ServicioSocial/alumnos/urls.py
@@ -4,7 +4,8 @@ from alumnos import views
 urlpatterns = [
     path('', views.AlumnosList.as_view(), name='lista_alumnos'),
     path('nuevo_alumno', views.NuevoAlumno.as_view(), name='nuevo_alumno'),
-    path('eliminar/<str:pk>', views.EliminarAlumno.as_view(), name='eliminar_alumno'),
     path('editar/<str:pk>', views.EditarAlumno.as_view(), name='editar_alumno'),
+    path('eliminar/<str:pk>', views.EliminarAlumno.as_view(), name='eliminar_alumno'),
+    
     
 ]
